6bdc611ba8bfd5c9c067f4146ff079878b012e96
Daniel Cotton <dan@daniel-cotton.co.uk>
2023-07-25 22:42:50 +0100

    feat(instruction): implement baseclass with unimplemented execute method

--- packages/martian-robots/src/lib/model/instruction/index.ts
+++ packages/martian-robots/src/lib/model/instruction/index.ts
@@ -1,3 +1,8 @@
+import { Orientation } from "../orientation";
+import { Position } from "../position";
+
 export class Instruction {
-  
+  execute(position: Position, orientation: Orientation) {
+    throw new Error('Method not implemented, use a specific "Instruction" implementation.');
+  }
 }
