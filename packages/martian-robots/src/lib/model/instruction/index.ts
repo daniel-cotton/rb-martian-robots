e433f3d89e0a72f27c4d3d5ecc0191249af7276a
Daniel Cotton <dan@daniel-cotton.co.uk>
2023-07-25 22:55:05 +0100

    chore(comment): add an explainer comment

--- packages/martian-robots/src/lib/model/instruction/index.ts
+++ packages/martian-robots/src/lib/model/instruction/index.ts
@@ -12,6 +12,8 @@ export abstract class Instruction {
 
   static LEFT = new (class extends Instruction {
     override execute(position: Position, orientation: Orientation) {
+      // Get Index of current orientation
+      // then get previous orientation in ordered list (NORTH -> WEST -> SOUTH -> EAST -> NORTH)
       const orientationIndex = ORIENTATION_ORDERED.indexOf(orientation);
       const newOrientationIndex = orientationIndex > 0 ? orientationIndex - 1 : (ORIENTATION_ORDERED.length - 1);
       const newOrientation = ORIENTATION_ORDERED[newOrientationIndex];
@@ -21,6 +23,8 @@ export abstract class Instruction {
 
   static RIGHT = new (class extends Instruction {
     override execute(position: Position, orientation: Orientation) {
+      // Get Index of current orientation
+      // then get next orientation in ordered list (NORTH -> EAST -> SOUTH -> WEST -> NORTH)
       const orientationIndex = ORIENTATION_ORDERED.indexOf(orientation);
       const newOrientationIndex = (orientationIndex + 1) % ORIENTATION_ORDERED.length;
       const newOrientation = ORIENTATION_ORDERED[newOrientationIndex];
