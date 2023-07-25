ab4b50038a200db8c180f316657f5f9d144e8578
Daniel Cotton <dan@daniel-cotton.co.uk>
2023-07-25 22:46:02 +0100

    feat(abstract): implement abstract class via typescript and initial LEFT instruction

--- packages/martian-robots/src/lib/model/instruction/index.ts
+++ packages/martian-robots/src/lib/model/instruction/index.ts
@@ -1,8 +1,23 @@
 import { Orientation } from "../orientation";
 import { Position } from "../position";
 
-export class Instruction {
-  execute(position: Position, orientation: Orientation) {
-    throw new Error('Method not implemented, use a specific "Instruction" implementation.');
-  }
+const ORIENTATION_ORDERED = [
+  Orientation.NORTH,
+  Orientation.EAST,
+  Orientation.SOUTH,
+  Orientation.WEST
+]
+
+export abstract class Instruction {
+
+  static LEFT = new (class extends Instruction {
+    override execute(position: Position, orientation: Orientation) {
+      const orientationIndex = ORIENTATION_ORDERED.indexOf(orientation);
+      const newOrientationIndex = (orientationIndex - 1) % 4;
+      const newOrientation = ORIENTATION_ORDERED[newOrientationIndex];
+      return { position, orientation: newOrientation };
+    }
+  })();
+
+  abstract execute(position: Position, orientation: Orientation): { position: Position, orientation: Orientation };
 }
