c74739361392996c3909d70228423b312ae417eb
Daniel Cotton <dan@daniel-cotton.co.uk>
2023-07-25 20:18:17 +0100

    feat(world-tests): implement world, robot, scent stubs and world unit tests

---
+++ packages/martian-robots/src/lib/model/world/index.ts
@@ -0,0 +1,24 @@
+
+/**
+ * An implementation of the world in which the robots move.
+ * 
+ * @class World
+ * @property {number} width - The width of the world
+ * @property {number} height - The height of the world
+ * 
+ * The above are the upper-right coordinates of the rectangular 
+ * world, the lower-left coordinates are assumed to be 0, 0.
+ * 
+ * Illustration:
+ * 
+ * | - | - | - | - | - | (width, height)
+ * | - | - | - | - | - |
+ * | - | - | - | - | - |
+ * | - | - | - | - | - |
+ * | - | - | - | - | - |
+ * | - | - | - | - | - |
+ * (0, 0)
+ * 
+ */
+export class World {
+}
