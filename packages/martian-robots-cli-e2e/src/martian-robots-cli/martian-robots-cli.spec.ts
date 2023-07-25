6576611ccb0b0cae301abf6c82fb3797128513a5
Daniel Cotton <dan@daniel-cotton.co.uk>
2023-07-26 00:18:23 +0100

    feat(cli-e2e): implement failing cli e2e tests

--- packages/martian-robots-cli-e2e/src/martian-robots-cli/martian-robots-cli.spec.ts
+++ packages/martian-robots-cli-e2e/src/martian-robots-cli/martian-robots-cli.spec.ts
@@ -1,12 +1,26 @@
 import { execSync } from 'child_process';
 import { join } from 'path';
 
-describe('CLI tests', () => {
-  it('should print a message', () => {
+describe('MartianRobotsCLI tests', () => {
+  it('given valid input, should respond with expected robot positions', () => {
+    const sampleInput = `5 3
+1 1 E
+RFRFRFRF
+
+3 2 N
+FRRFLLFFRRFLL
+
+0 3 W
+LLFFFLFLFL`;
+
+    const expectedOutput = `1 1 E
+3 3 N LOST
+2 3 S`;
+
     const cliPath = join(process.cwd(), 'dist/packages/martian-robots-cli');
 
-    const output = execSync(`node ${cliPath}`).toString();
+    const output = execSync(`echo "${sampleInput.split("\n").join("\\\n")}" | node ${cliPath}`).toString();
 
-    expect(output).toMatch(/Hello World/);
+    expect(output).toMatch(expectedOutput);
   });
 });
