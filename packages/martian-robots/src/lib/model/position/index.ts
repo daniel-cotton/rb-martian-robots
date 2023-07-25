68af3fecff615656f6c4f3ded3d38a5fece93e04
Daniel Cotton <dan@daniel-cotton.co.uk>
2023-07-25 22:06:23 +0100

    feat(position): implement position class

--- packages/martian-robots/src/lib/model/position/index.ts
+++ packages/martian-robots/src/lib/model/position/index.ts
@@ -1,2 +1,11 @@
 export class Position {
+  x: number;
+  y: number;
+
+  constructor({ x, y } : { x: number, y: number }) {
+    if (x > 50 || y > 50) throw new Error('X / Y Coordinates must both be less than 50.');
+    if (x < 0 || y < 0) throw new Error('X / Y Coordinates must be positive.');
+    this.x = x;
+    this.y = y;
+  }
 }
